4a357912f95a4cdc1b4c48e5a1737eb5b7bf8d44
crazyed <>
2001-10-09 04:27:09 +0000

    bugfix:  /stack push+pop of an unset /set will now restore it to its original value, ie, unset.

--- source/vars.c
+++ source/vars.c
@@ -1060,7 +1060,7 @@ void do_stack_set(int type, char *args)
 			aptr->set = (IrcVariable *) new_malloc(sizeof(IrcVariable));
 			memcpy(aptr->set, &irc_variable[var_index], sizeof(IrcVariable));
 			aptr->name = m_strdup(irc_variable[var_index].name);
-			aptr->set->string = m_strdup(irc_variable[var_index].string);
+			aptr->set->string = (irc_variable[var_index].string) ? m_strdup(irc_variable[var_index].string) : NULL;
 			aptr->var_index = var_index;
 		}
 		else
