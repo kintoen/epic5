02facdfe991e5cb005ef49acc16a1ca324a69f29
crazyed <>
2001-10-10 22:47:17 +0000

    * Minor optimisation to find_array_item(). * Fix to the "rolling nick" bug occasionally seen for lagged users.

--- source/numbers.c
+++ source/numbers.c
@@ -646,7 +646,9 @@ void 	numbered_command (char *from, int comm, char **ArgList)
 	}
 	case 432:		/* #define ERR_ERRONEUSNICKNAME 432 */
 	{
-		if (get_int_var(AUTO_NEW_NICK_VAR))
+		if (!my_stricmp(user, ArgList[0]))
+			yell("WARNING:  Strange invalid nick message received.  You are probably lagged.");
+		else if (get_int_var(AUTO_NEW_NICK_VAR))
 			fudge_nickname(from_server);
 		else
 			reset_nickname(from_server);
@@ -750,7 +752,10 @@ void 	numbered_command (char *from, int comm, char **ArgList)
 	case 438:		/* EFnet/TS4 "nick collision" numeric 438 */
 	case 453:		/* EFnet/TS4 "nickname lost" numeric 453 */
 	{
-		if (get_int_var(AUTO_NEW_NICK_VAR))
+		if (!my_stricmp(user, ArgList[0]))
+			/* This should stop the "rolling nicks" in their tracks. */
+			yell("WARNING:  Strange invalid nick message received.  You are probably lagged.");
+		else if (get_int_var(AUTO_NEW_NICK_VAR))
 			fudge_nickname(from_server);
 		else
 			reset_nickname(from_server);
