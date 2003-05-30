57715f140bc49b8bb0f6b42eefb13f75ee400b18
jnelson <>
2003-05-30 19:58:10 +0000

    Commit 519 * Change m_strcat_ues_c to take a (char *) "unescape" param and not (int) * Rename m_strcat_ues_c to malloc_strcat_ues_c * Rewrite malloc_strcat_ues_c to support new semantics. * Make sure not to pass 0 to alloca() in ctcp checks * Initialize (DCC)->server [found by valgrind] * Change 'expand_alias' so 'unescape' is (char *) and not (int) * Add extra sanity checks in mangle_line against buffer overruns * Fix window_channel() to use new malloc_strcat_ues() arguments.

--- source/ctcp.c
+++ source/ctcp.c
@@ -1,4 +1,4 @@
-/* $EPIC: ctcp.c,v 1.23 2003/05/09 22:31:14 jnelson Exp $ */
+/* $EPIC: ctcp.c,v 1.24 2003/05/17 18:30:21 crazyed Exp $ */
 /*
  * ctcp.c:handles the client-to-client protocol(ctcp). 
  *
@@ -953,8 +953,8 @@ void	send_ctcp (int type, const char *to, int datatag, const char *format, ...)
 	int	len;
 
 	/* Make sure that the final \001 doesnt get truncated */
-	if ((len = IRCD_BUFFER_SIZE - (12 + strlen(to))) < 0)
-		return;				/* Whatever! */
+	if ((len = IRCD_BUFFER_SIZE - (12 + strlen(to))) <= 0)
+		return;				/* Whatever. */
 	putbuf2 = alloca(len);
 
 	if (format)
