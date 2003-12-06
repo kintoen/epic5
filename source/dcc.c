f970c07c27599e5374226f5c635601f5f04e5532
crazyed <>
2003-12-06 14:34:33 +0000

    * Fix DCC events so that they occur in the context of "no server" (FROMSERV)   rather than the "current server" (NOSERV).  This can fix or completely break   certain intermittent scripting problems. * Fixed {f,}q1cmd in commandqueues so that it works properly in the "no server"   context.

--- source/dcc.c
+++ source/dcc.c
@@ -1,4 +1,4 @@
-/* $EPIC: dcc.c,v 1.81 2003/12/01 04:41:34 crazyed Exp $ */
+/* $EPIC: dcc.c,v 1.82 2003/12/03 05:21:11 jnelson Exp $ */
 /*
  * dcc.c: Things dealing client to client connections. 
  *
@@ -2446,7 +2446,7 @@ void	dcc_check (fd_set *Readables, fd_set *Writables)
 	    {
 		FD_CLR(Client->socket, Readables);	/* No more! */
 		previous_server = from_server;
-		from_server = NOSERV;
+		from_server = FROMSERV;
 
 		switch (Client->flags & DCC_TYPES)
 		{
