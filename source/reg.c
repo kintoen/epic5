7254d7bf92e533981ff288258a141d92125cc2ed
jnelson <>
2003-12-25 04:39:49 +0000

    * Fix matching of /on type "*" to always have a weight of 1. (rb adm)

--- source/reg.c
+++ source/reg.c
@@ -1,4 +1,4 @@
-/* $EPIC: reg.c,v 1.6 2003/05/09 04:29:52 jnelson Exp $ */
+/* $EPIC: reg.c,v 1.7 2003/12/23 02:36:05 jnelson Exp $ */
 /*
  * reg.c - "glob"-like wildcard pattern matching (not regexes)
  *
@@ -710,6 +710,8 @@ char *	pattern2regex (const char *pattern, int *weight)
 			break;
 	    }
 	}
+
+	(*weight)++;		/* The nul counts towards the weight */
 
 	if (x_debug & DEBUG_REGEX_DEBUG)
 		yell("[%s] -> [%s]", pattern, retval);
