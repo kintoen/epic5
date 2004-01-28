970a9a302ceb865aaec05ea068c517886723f925
crazyed <crazyed>
2004-01-28 09:51:46 +0000

    Fixed a thinko with the last DCC fix.

--- source/dcc.c
+++ source/dcc.c
@@ -1,4 +1,4 @@
-/* $EPIC: dcc.c,v 1.87 2004/01/27 04:41:39 crazyed Exp $ */
+/* $EPIC: dcc.c,v 1.88 2004/01/28 09:51:46 crazyed Exp $ */
 /*
  * dcc.c: Things dealing client to client connections. 
  *
@@ -1485,7 +1485,7 @@ static	void	dcc_getfile (char *args, int resume)
 			set_server_protocol_state(from_server, 0);
 			send_ctcp(CTCP_PRIVMSG, user, CTCP_DCC, 
 #if 1
-				strchr(dcc->description, space)
+				sindex(dcc->description, space)
 				? "RESUME \"%s\" %s %ld"
 				: "RESUME %s %s %ld", 
 				dcc->description,
--- source/irc.c
+++ source/irc.c
@@ -1,4 +1,4 @@
-/* $EPIC: irc.c,v 1.684 2004/01/27 04:41:39 crazyed Exp $ */
+/* $EPIC: irc.c,v 1.685 2004/01/28 09:51:46 crazyed Exp $ */
 /*
  * ircII: a new irc client.  I like it.  I hope you will too!
  *
@@ -52,7 +52,7 @@ const char internal_version[] = "20040125";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 689;
+const unsigned long	commit_id = 690;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
