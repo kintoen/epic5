1ecc1afbf33f51035ef4fc007721bcd07d4491de
jnelson <jnelson>
2004-03-29 15:38:14 +0000

    You know, there is really no excuse for me to make mistakes like this. This is just a very bad start to the day...

--- source/screen.c
+++ source/screen.c
@@ -1,4 +1,4 @@
-/* $EPIC: screen.c,v 1.68 2004/03/29 15:27:29 jnelson Exp $ */
+/* $EPIC: screen.c,v 1.69 2004/03/29 15:38:14 jnelson Exp $ */
 /*
  * screen.c
  *
@@ -1800,11 +1800,7 @@ const 	u_char	*ptr;
 				int	lhs_count = 0;
 				int	continued_count = 0;
 
-                                /* Because Blackjac asked me to */
-                                if (indent > max_cols / 3)
-                                        indent = max_cols / 3;
-
-				if (do_indent &&
+				if (do_indent && (indent < (max_cols / 3)) &&
 					((int)strlen(cont_ptr) < indent))
 				{
 					size_t size = indent + 10;;
