d91068419fa51ebaba067162cba1ac56e84b0268
jnelson <>
2004-07-28 01:02:39 +0000

    * Refactor IrcVariable to not depend on having the varname. * This will lead to multiple hard links to the same /set value in the future. * Nix the 'name' and 'hash' values in IrcVariable, no longer needed. * Make all uses of "realname" use the /set realname variable (rb adm) * Nix the global "realname" variable, use /set realname for all. * Fix bug with free_bucket() * Fix compile warnings in vars.c * Store built in variable xref table in a bucket. * When changing a user-created /set, start calling the user's scriptback * Allow the user's scriptback to call /set to override the user's value. * Tell the user that they created a new /set.

--- source/server.c
+++ source/server.c
@@ -1,4 +1,4 @@
-/* $EPIC: server.c,v 1.133 2004/06/28 23:48:15 jnelson Exp $ */
+/* $EPIC: server.c,v 1.134 2004/07/08 08:27:59 crazyed Exp $ */
 /*
  * server.c:  Things dealing with that wacky program we call ircd.
  *
@@ -1796,7 +1796,8 @@ void	register_server (int refnum, const char *nick)
 	send_to_aserver(refnum, "USER %s %s %s :%s", username, 
 			(send_umode && *send_umode) ? send_umode : 
 			(LocalHostName ? LocalHostName : hostname), 
-			username, (*realname ? realname : space));
+			username, (get_string_var(REALNAME_VAR) ? 
+				   get_string_var(REALNAME_VAR) : space));
 	change_server_nickname(refnum, nick);
 	if (x_debug & DEBUG_SERVER_CONNECT)
 		yell("Registered with server [%d]", refnum);
