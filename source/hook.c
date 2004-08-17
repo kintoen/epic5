4139beaeb9ac45db5661c2365945067dd00b8584
crazyed <>
2004-08-17 16:09:46 +0000

    * Numerous new and changed features as per UPDATES. * Better handling of damaged /who x,y responses.

--- source/hook.c
+++ source/hook.c
@@ -1,4 +1,4 @@
-/* $EPIC: hook.c,v 1.40 2004/06/28 23:48:15 jnelson Exp $ */
+/* $EPIC: hook.c,v 1.41 2004/08/11 23:58:39 jnelson Exp $ */
 /*
  * hook.c: Does those naughty hook functions. 
  *
@@ -141,7 +141,7 @@ Hookables hook_function_templates[] =
 	{ "EXEC_EXIT",		(Hook *) 0,	3,	0,	0 },
 	{ "EXEC_PROMPT",	(Hook *) 0,	2,	0,	0 },
         { "EXIT",               (Hook *) 0,     1,      0,      0 },
-	{ "FLOOD",		(Hook *) 0,	3,	0,	0 },
+	{ "FLOOD",		(Hook *) 0,	5,	0,	0 },
 	{ "GENERAL_NOTICE",	(Hook *) 0,	3,	0,	0 },
 	{ "GENERAL_PRIVMSG",	(Hook *) 0,	3,	0,	0 },
 	{ "HELP",		(Hook *) 0,	2,	0,	0 },
--- source/ircaux.c
+++ source/ircaux.c
@@ -1,4 +1,4 @@
-/* $EPIC: ircaux.c,v 1.115 2004/08/08 03:52:50 jnelson Exp $ */
+/* $EPIC: ircaux.c,v 1.116 2004/08/11 23:58:39 jnelson Exp $ */
 /*
  * ircaux.c: some extra routines... not specific to irc... that I needed 
  *
@@ -3427,6 +3427,18 @@ char	*dequote_it (char *str, size_t *len)
 	*ptr = '\0';
 	*len = new_size;
 	return (buffer);
+}
+
+/*
+ * As above, but act as if the malloc never occured.  dequote_it should
+ * probably be written in terms of dequote_buffer for performance purposes.
+ */
+char	*dequote_buffer (char *str, size_t *len)
+{
+	char	*malloc = dequote_it(str, len);
+	char	*ret = memmove(str, malloc, 1 + *len);
+	new_free(&malloc);
+	return ret;
 }
 
 unsigned char isspace_table [256] = 
