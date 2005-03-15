af7e1e0a8df118583e17ef1e909a8d318841fca8
jnelson <>
2005-03-15 05:36:20 +0000

    * Keep timestamps along with lastlog entries. * Add $line(<num> -TIME), return the timestamp when <num> was added to lastlog. * Remove some #define's from config.h that are not referenced any longer. * Remove #define QUIT_ON_OPERATOR_KILL because 1 line of script defeats it.

--- source/glob.c
+++ source/glob.c
@@ -1,6 +1,6 @@
-/* $EPIC: glob.c,v 1.9 2003/07/10 13:08:57 jnelson Exp $ */
+/* $EPIC: glob.c,v 1.10 2003/07/22 21:12:54 jnelson Exp $ */
 #include "config.h"
-#if defined(NEED_GLOB)
+#ifdef NEED_GLOB
 
 /*
  * Copyright (c) 1989, 1993
