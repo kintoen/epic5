82ebb5fc765a81f6280b02132269e6dd70b4eda3
jnelson <>
2005-04-15 02:47:45 +0000

    * Make /bind "compress" string, and bind and print it compressed. * This fixes /bind'ing literal characters being displayed literally. * Round 3 of blackjac's builtins script * Round 1 of fudd's loadformats script. * Fix some "all-the-world's-an-int" bugs reported by xlc (aix's compiler).

--- source/network.c
+++ source/network.c
@@ -1,4 +1,4 @@
-/* $EPIC: network.c,v 1.63 2005/03/04 00:57:45 jnelson Exp $ */
+/* $EPIC: network.c,v 1.64 2005/03/19 03:55:55 jnelson Exp $ */
 /*
  * network.c -- handles stuff dealing with connecting and name resolving
  *
@@ -648,7 +648,7 @@ static int	set_blocking (int fd)
  * has been closed in the interim.  This wrapper for accept() attempts to
  * defeat this by making the accept() call nonblocking.
  */
-int	Accept (int s, SA *addr, int *addrlen)
+int	Accept (int s, SA *addr, socklen_t *addrlen)
 {
 	int	retval;
 
