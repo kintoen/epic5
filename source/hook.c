29c51739cb955947cb72d2ef73f3d1ae4105f846
jnelson <>
2006-09-24 16:03:58 +0000

    * Fixes for two memory leaks from nullie * New /on, /ON WINDOW_SERVER, from nullie who needs to write a blurb.

--- source/hook.c
+++ source/hook.c
@@ -1,4 +1,4 @@
-/* $EPIC: hook.c,v 1.66 2006/09/19 12:13:36 jnelson Exp $ */
+/* $EPIC: hook.c,v 1.67 2006/09/21 12:51:16 jnelson Exp $ */
 /*
  * hook.c: Does those naughty hook functions. 
  *
@@ -231,6 +231,7 @@ Hookables hook_function_templates[] =
 	{ "WINDOW_CREATE",	NULL,	1, 	0,	0,	NULL },
 	{ "WINDOW_BEFOREKILL",	NULL,	1,	0,	0,	NULL },
 	{ "WINDOW_KILL",	NULL,	2,	0,	0,	NULL },
+	{ "WINDOW_SERVER",	NULL,	3,	0,	0,	NULL },
 	{ "YELL",		NULL,	1,	0,	0,	NULL },
 };
 
