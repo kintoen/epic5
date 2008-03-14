3111cc8a8873255a623413906bfbc2065763b69a
jnelson <jnelson>
2008-03-14 22:18:37 +0000

    * sig.inc should be created in the include directory. (rb larne) * Fix build on systems where CFLAGS doesn't get set (larne)

--- source/irc.c
+++ source/irc.c
@@ -1,4 +1,4 @@
-/* $EPIC: irc.c,v 1.761 2008/03/14 00:12:53 jnelson Exp $ */
+/* $EPIC: irc.c,v 1.762 2008/03/14 22:18:39 jnelson Exp $ */
 /*
  * ircII: a new irc client.  I like it.  I hope you will too!
  *
@@ -52,7 +52,7 @@ const char internal_version[] = "20070916";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 762;
+const unsigned long	commit_id = 763;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
