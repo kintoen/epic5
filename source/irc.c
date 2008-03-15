9ee7f0a220ff5b7867cd345ecd83d4d5155cab90
jnelson <jnelson>
2008-03-15 20:47:35 +0000

    Fix a comesmetic configure bug for larne

--- source/irc.c
+++ source/irc.c
@@ -1,4 +1,4 @@
-/* $EPIC: irc.c,v 1.763 2008/03/15 01:40:06 jnelson Exp $ */
+/* $EPIC: irc.c,v 1.764 2008/03/15 20:47:40 jnelson Exp $ */
 /*
  * ircII: a new irc client.  I like it.  I hope you will too!
  *
@@ -52,7 +52,7 @@ const char internal_version[] = "20070916";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 764;
+const unsigned long	commit_id = 765;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
