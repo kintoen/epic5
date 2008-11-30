efed31d5e903f305e595a645c1ee8859f3e64a02
jnelson <>
2008-11-30 19:09:23 +0000

    * Calls to next_expr() must be prepared to handle NULL return values

--- source/alias.c
+++ source/alias.c
@@ -1,4 +1,4 @@
-/* $EPIC: alias.c,v 1.84 2007/04/12 03:24:14 jnelson Exp $ */
+/* $EPIC: alias.c,v 1.85 2008/11/28 16:28:03 jnelson Exp $ */
 /*
  * alias.c -- Handles the whole kit and caboodle for aliases.
  *
@@ -715,6 +715,9 @@ ArgList	*parse_arglist (char *arglist)
 	char *	modifier, *value;
 	int	arg_count = 0;
 	ArgList *args = new_malloc(sizeof(ArgList));
+
+	if (arglist == NULL)
+		panic(1, "parse_arglist: arglist is NULL and it shouldn't be.");
 
 	args->void_flag = args->dot_flag = 0;
 	for (this_term = arglist; *this_term; this_term = next_term)
--- source/commands.c
+++ source/commands.c
@@ -1,4 +1,4 @@
-/* $EPIC: commands.c,v 1.184 2008/11/26 03:26:34 jnelson Exp $ */
+/* $EPIC: commands.c,v 1.185 2008/11/28 16:28:03 jnelson Exp $ */
 /*
  * commands.c -- Stuff needed to execute commands in ircII.
  *		 Includes the bulk of the built in commands for ircII.
@@ -3342,7 +3342,10 @@ static void	eval_inputlist (char *args, char *line)
 {
         char *tmp;
 	if (args[0]=='(') {
-                tmp=next_expr(&args, '(');
+                if (!(tmp = next_expr(&args, '('))) {
+			yell("INPUT: syntax error with arglist");
+			return;
+		}
                 runcmds_with_arglist(args, tmp, line);
 	} else {
                 /* traditional behavior */
