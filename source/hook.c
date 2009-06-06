c75fa71b294f877986b7201c828722243305443f
jnelson <>
2009-06-06 19:20:57 +0000

    * Don't lamely malloc_strcpy() over non-malloced strings in hookctl.

--- source/hook.c
+++ source/hook.c
@@ -1,4 +1,4 @@
-/* $EPIC: hook.c,v 1.82 2008/11/27 03:44:11 jnelson Exp $ */
+/* $EPIC: hook.c,v 1.83 2009/06/05 21:21:35 jnelson Exp $ */
 /*
  * hook.c: Does those naughty hook functions. 
  *
@@ -2685,7 +2685,11 @@ char *hookctl (char *input)
 			curhook = curhook->under;
 		if (!curhook)
 			RETURN_INT(0);
-		malloc_strcpy(&curhook->buffer, input);
+		if (curhook->buffer_changed)
+			malloc_strcpy(&curhook->buffer, input);
+		else
+			curhook->buffer = malloc_strdup(input);
+		curhook->buffer_changed = 1;
 		RETURN_INT(1);
 		break;
 
