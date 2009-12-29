75b0b9d2bbceef0792a376792918ed83d6828ffd
jnelson <>
2009-12-29 04:20:14 +0000

    * Fix the redonkulous spurious errors when using $convert(). * Add a configure check for strtoimax() being a macro (ugh) * Apparently strings in configure are supposed to be [quote]d.

--- source/network.c
+++ source/network.c
@@ -1,4 +1,4 @@
-/* $EPIC: network.c,v 1.83 2008/04/05 00:20:38 jnelson Exp $ */
+/* $EPIC: network.c,v 1.84 2009/10/29 07:37:32 jnelson Exp $ */
 /*
  * network.c -- handles stuff dealing with connecting and name resolving
  *
@@ -526,10 +526,15 @@ int	inet_ptohn (int family, const char *ip, char *retval, int size)
  */
 int	one_to_another (int family, const char *what, char *retval, int size)
 {
+	/* XXX I wish this wasn't necessary */
+	int	old_window_display = window_display;
+	window_display = 0;
+
 	if (inet_ptohn(family, what, retval, size))
 	{
 		if (inet_hntop(family, what, retval, size))
 		{
+			window_display = old_window_display;
 			syserr(-1, "one_to_another: both inet_ptohn and "
 					"inet_hntop failed (%d,%s)", 
 					family, what);
@@ -537,6 +542,7 @@ int	one_to_another (int family, const char *what, char *retval, int size)
 		}
 	}
 
+	window_display = old_window_display;
 	return 0;
 }
 
