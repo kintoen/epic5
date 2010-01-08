0d982fa1e2f783a2be2b5f2ee6f9b482dcb96179
jnelson <>
2010-01-08 05:15:03 +0000

    * Improve the test for libarchive in configure * Fix /bind -default because it whacked the bindings themselves. doh!

--- source/keys.c
+++ source/keys.c
@@ -1,4 +1,4 @@
-/* $EPIC: keys.c,v 1.57 2008/11/26 03:26:34 jnelson Exp $ */
+/* $EPIC: keys.c,v 1.58 2009/06/18 04:46:53 jnelson Exp $ */
 /*
  * keys.c:  Keeps track of what happens whe you press a key.
  *
@@ -1095,6 +1095,7 @@ BUILT_IN_COMMAND(bindcmd) {
     if (*seq == '-') {
 	if (!my_strnicmp(seq + 1, "DEFAULTS", 1)) {
 	    remove_bindings();
+	    init_binds();
 	    init_keys();
 	    init_termkeys();
 	    return;
