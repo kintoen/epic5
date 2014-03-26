01557cbffa405b540d4db4fc1ac2eb7413a3852c
jnelson <>
2014-03-26 20:44:57 +0000

    * Write a half-finished reimplementation of $fix_width() that is commented out * Fix rcpindex() when the search char isn't in the string * Fix search_for() which makes $before() and $after() utf8 aware. * Eliminate usage of internal sindex() -> strchr(), strpbrk(), or cpindex(). * Rewrite/ensure these functions are UTF8 aware:         strip   split   before  after   curpos

--- include/ircaux.h
+++ include/ircaux.h
@@ -167,7 +167,7 @@ char *	endstr			(char *);
 ssize_t searchbuf		(const unsigned char *, size_t, size_t, int);
 int	remove_from_comma_list	(char *str, const char *what);
 char *	dequote_buffer		(char *str, size_t *len);
-char *	fix_string_width	(const char *, int, char, size_t);
+char *	fix_string_width	(const char *, int, int, size_t);
 int     check_xdigit 		(unsigned char digit);
 
 void	add_mode_to_str		(char *, size_t, int);
